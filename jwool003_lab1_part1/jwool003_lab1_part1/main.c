48076b82b2deca8e31a03a5e405fd816f7758684
jonathanwoolf <jwool003@ucr.edu>
2018-07-30 21:27:49 -0700

    part 2 done

---
+++ jwool003_lab1_part1/jwool003_lab1_part1/main.c
@@ -0,0 +1,35 @@
+/*
+ * jwool003_lab1_part1.c
+ *
+ * Created: 7/30/2018 8:50:32 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
+    // initialize to 0s
+    unsigned char tmpB = 0x00; 	// Temporary variable to hold the value of B
+    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    while(1)
+    {
+        // (1) Read input
+        tempA = PINA & 0x03;
+        // (2) Perform computation
+        // if PA0 is 1, set PB1PB0=01, else=10
+        if (tmpA == 0x01) { // True if PA0 is 1
+            tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+            // (clear rightmost 2 bits, then set to 01)
+        }
+        else {
+            tmpB = (tmpB & 0xFC) | 0x00; // Sets tmpB to bbbbbb10
+            // (clear rightmost 2 bits, then set to 10)
+        }
+        // (3) Write output
+        PORTB = tmpB;
+    }
+    return 0;
+}
---
+++ jwool003_lab1_part2/jwool003_lab1_part2/main.c
@@ -0,0 +1,29 @@
+/*
+ * jwool003_lab1_part2.c
+ *
+ * Created: 7/30/2018 8:53:58 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRC  = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs,
+    // initialize spaces to 4 and the rest to 0s
+	unsigned char spaces = 0x04; // How many spaces total there are
+    unsigned char cntavail = 0x00; 	// How many available spaces there are
+    unsigned char occupado = 0x00; // Temporary variable to hold the value of A
+    while(1)
+    {
+        // (1) Read inputs and returns a 1 for PA0 through PA3 if occupied MOFO
+        occupado = (PINA & 0x01) + ((PINA >> 0x01) & 0x01) + ((PINA >> 0x02) & 0x01) + ((PINA >> 0x03) & 0x01);
+        // (2) Perform computation
+		cntavail = spaces - occupado; //finds out the available amount of spaces
+        // (3) Write output
+        PORTC = cntavail;
+    }
+    return 0;
+}
+
