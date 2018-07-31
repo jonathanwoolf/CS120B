b544174b68ab63b5ebdd844148e8cdd3ef779fe8
jonathanwoolf <jwool003@ucr.edu>
2018-07-30 20:42:28 -0700

    Part 1 is done

---
+++ jwool003@ucr.edu/jwool003@ucr.edu/main.c
@@ -0,0 +1,35 @@
+/*
+ * jwool003@ucr.edu.c
+ *
+ * Created: 7/30/2018 8:36:52 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
+    // initialize to 0s
+    unsigned char tmpB = 0x00; 	// Temporary variable to hold the value of B
+    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    while(1)
+    {
+        // (1) Read input
+        tmpA = PINA & 0x03;
+        // (2) Perform computation
+        // if PA0 is 1, set PB1PB0=01, else=10
+        if (tmpA == 0x01) { // True if PA0 is 1
+            tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+            // (clear rightmost 2 bits, then set to 01)
+        }
+        else {
+            tmpB = (tmpB & 0xFC) | 0x00; // Sets tmpB to bbbbbb10
+            // (clear rightmost 2 bits, then set to 00)
+        }
+        // (3) Write output
+        PORTB = tmpB;
+    }
+    return 0;
+}
