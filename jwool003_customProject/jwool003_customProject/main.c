f3ac14377bcb2efd1d591ab89ea6965a8f8916ee
jonathanwoolf <jwool003@ucr.edu>
2018-08-28 22:47:58 -0700

    Updated obstacles and coments. Nothing broken but overDrive doesn't work

--- jwool003_customProject/jwool003_customProject/main.c
+++ jwool003_customProject/jwool003_customProject/main.c
@@ -24,19 +24,20 @@ unsigned char column_sel; // grounds column to display pattern
 unsigned char char_location = 0x10; // Location of the player character
 unsigned char char_sel = 0x7F; // The bottom column where the character resides
 unsigned char startGame = 0; // starts the game
-unsigned char score = 0;
-unsigned char score1 = 0; // The player's score
+unsigned char score = 0; // The player's score
+unsigned char score1 = 0; 
 unsigned char score10 = 0;
 unsigned char score100 = 0;
-unsigned char highscore;
-unsigned char highscore1; // The player's score
-unsigned char highscore10;
-unsigned char highscore100;
+unsigned char highscore = 0; // The system's high score
+unsigned char highscore1 = 0; 
+unsigned char highscore10 = 0;
+unsigned char highscore100 = 0;
+	
+const unsigned char course[35] = {0x81, 0x81, 0xE7, 0x81, 0x81, 0x81, 0xE2, 0x62, 0x24, 0x34, 0x2E, 0xA4, 0x24, 0x22, 0x14, 0x12, 0x0A, 0x09, 0xAB, 0xC3, 0x55, 0xBA, 0xAA, 0xAB, 0x55, 0x33, 0xAA,
+0xE7, 0x55, 0xC3, 0xAA, 0xE7, 0x55, 0xC3, 0xAA}; // This is the obstacle course
 
-const unsigned char course[35] = {0x81, 0x00, 0xE7, 0x00, 0x00, 0x81, 0xE2, 0x62, 0x24, 0x34, 0x2E, 0xA4, 0x24, 0x22, 0x14, 0x12, 0x0A, 0x09, 0xAB, 0xC3, 0x55, 0xBA, 0xAA, 0xAB, 0x55, 0x00, 0xAA,
-0xE7, 0x55, 0xC3, 0xAA, 0xE7, 0x55, 0xC3, 0xAA};
-unsigned char customChar1[8] = {0x0A, 0x15, 0x1B, 0x15, 0x15, 0x15, 0x0A, 0x0E};
-unsigned char customChar2[8] = {0x15, 0x0A, 0x04, 0x0A, 0x0A, 0x0A, 0x15, 0x11};
+unsigned char customChar1[8] = {0x0A, 0x15, 0x1B, 0x15, 0x15, 0x15, 0x0A, 0x0E}; // Inverted custom character
+unsigned char customChar2[8] = {0x15, 0x0A, 0x04, 0x0A, 0x0A, 0x0A, 0x15, 0x11}; // This is the non-inverted one ;)
 
 // ====================
 // SM1: Moves your character on the matrix
@@ -46,6 +47,12 @@ int Character_Tick(int state)
 {		
 	switch (Character_State) {
 		case button:
+// 			if(overDrive) // THIS IS VERY BROKEN
+// 			{
+// 				LCD_Cursor(2); LCD_WriteData('H' + '0');
+// 				LCD_Cursor(3); LCD_WriteData('E' + '0');
+// 				LCD_Cursor(4); LCD_WriteData('Y' + '0');
+// 			}
 			if(leftButton && !rightButton) // Character moves to the left to avoid obstacles
 			{ 
 				if(char_location != 0x01) { char_location = char_location >> 1;} // Obviously a right shift must occur
@@ -84,34 +91,49 @@ enum Obstacles_States {start, obstacleCourse, delay} Obstacles_State;
 int Obstacles_Task(int state)
 {
 	//static char count = 0; 
+	static unsigned char flag = 0; // Flag to switch between courses
 	static unsigned char i = 0; // Iterate through course
 	
 	switch (Obstacles_State){
 		case start:
-			column_sel = 0xFE; // Initializes start point for course
-			column_val = course[i]; // assign course to column_val
-			if(startGame) { Obstacles_State = obstacleCourse;} // Start generating obstacles when user hits both buttons
-			else { Obstacles_State = start;} // Wait for user to start the game
-			break;
-		case obstacleCourse:   // Generate obstacles
-			if(column_sel == 0x7F) { column_sel = 0xFE; column_val = course[i]; i++;} // If obstacle reaches the character return to the top of the matrix and iterate through course
-			if(i == 36) { i = 4;} // If the end of the course is reached, reset iterator enough to avoid major delay
-			else {
-				column_sel = (column_sel << 1) | 0x01; // Move obstacle column down toward the character and "or" with 0x01 in order to only display one column at a time
+		flag = 0;
+		column_sel = 0xFE; // Initializes start point for course
+		column_val = course[i]; // assign course to column_val
+		if(startGame) { Obstacles_State = obstacleCourse;} // Start generating obstacles when user hits both buttons
+		else { Obstacles_State = start;} // Wait for user to start the game
+		break;
+	case obstacleCourse:   // Generate obstacles
+		if(column_sel == 0x7F) // If obstacle reaches the character return to the top of the matrix and iterate through course
+		{
+			column_sel = 0xFE;
+			if(flag == 0) // Display standard course
+			{
+				column_val = course[i];
+			} 
+			else // Display inverted course after standard course is complete
+			{
+				column_val = ~course[i];
 			}
-			Obstacles_State = obstacleCourse;
-			if(!startGame) { i = 0; Obstacles_State = start;} // Go to start and await soft reset 
- 			//Obstacles_State = (overDrive == 1)? obstacleCourse : delay;
-			break;
-		case delay: // Fix
-// 			if(count < 2) { count++; Obstacles_State = delay;}
-// 			else { count = 0; Obstacles_State = obstacleCourse;}
-			break;
-		default:
-			Obstacles_State = obstacleCourse; // A default state can only transition and will never allow you to set a value
-			break;
+			i++;
+		}
+		if(i == 36) { i = 6; flag = !flag;} // If the end of the course is reached, reset iterator at beginning of standard course
+		else 
+		{
+			column_sel = (column_sel << 1) | 0x01; // Move obstacle column down toward the character and "or" with 0x01 in order to only display one column at a time
+		}
+		Obstacles_State = obstacleCourse; // Continue to iterate obstacle course
+		if(!startGame) { i = 0; Obstacles_State = start;} // Go to start and await soft reset
+		//Obstacles_State = (overDrive == 1)? obstacleCourse : delay;
+		break;
+	case delay: // Fix
+		// 			if(count < 2) { count++; Obstacles_State = delay;}
+		// 			else { count = 0; Obstacles_State = obstacleCourse;}
+		break;
+	default:
+		Obstacles_State = obstacleCourse; // A default state can only transition and will never allow you to set a value
+		break;
 	}
-	return Obstacles_State;		
+	return Obstacles_State;
 };
 
 // ====================
@@ -149,14 +171,14 @@ int Score_Task(int state)
 {	
 	switch(Score_State){
 		case scoreStart: 
-			Score_State = scoreStart;
+			Score_State = scoreStart; // Default state when the game is not running
 			if(startGame)
 			{ 
-				score100 = 0;
+				score100 = 0; // Sets all score values to 0
 				score10 = 0;
 				score1 = 0;
 				score = 0;
-				LCD_Cursor(2); LCD_WriteData(score100 + '0'); 
+				LCD_Cursor(2); LCD_WriteData(score100 + '0'); // Write each score value to the display from 100th place to 1th place
 				LCD_Cursor(3); LCD_WriteData(score10 + '0'); 
 				LCD_Cursor(4); LCD_WriteData(score1 + '0'); 
 				Score_State = scoreDisplay;
@@ -165,18 +187,18 @@ int Score_Task(int state)
 		case scoreDisplay: // Displays game and iterates score
 			if(startGame)
 			{
-				score++;
-				score1++;
-				if(score1 > 9) {score1 -= 10; score10++;}
-				if(score10 > 9) {score10 -= 10; score100++;}
-				LCD_Cursor(2); LCD_WriteData(score100 + '0');
+				score++; // Iterate actual score
+				score1++; // Iterate 1th place score
+				if(score1 > 9) {score1 -= 10; score10++;} // Iterate 10th place score
+				if(score10 > 9) {score10 -= 10; score100++;} // Iterate 100th place score
+				LCD_Cursor(2); LCD_WriteData(score100 + '0'); //Write each score value to the display from 100th place to 1th place
 				LCD_Cursor(3); LCD_WriteData(score10 + '0');
 				LCD_Cursor(4); LCD_WriteData(score1 + '0');
 				Score_State = scoreDisplay;
 			}
 			if(!startGame)
 			{
-				if(score > highscore)
+				if(score > highscore) // If the high score has been beaten replace it with the current score 
 				{
 					highscore100 = score100;
 					highscore10 = score10;
@@ -184,7 +206,7 @@ int Score_Task(int state)
 					highscore = score;
 				}
 				
-				eeprom_write_byte((uint8_t*)3, highscore100);
+				eeprom_write_byte((uint8_t*)3, highscore100); // Write high score to memory
 				eeprom_write_byte((uint8_t*)4, highscore10);
 				eeprom_write_byte((uint8_t*)5, highscore1);
 				eeprom_write_byte((uint8_t*)6, highscore);
@@ -206,33 +228,32 @@ int main(void)
 	DDRC = 0xFF;  PORTC = 0x00; // LCD Output
 	DDRD = 0xC0;  PORTD = 0x3F; // Button Input
 	
-	LCD_init();
+	LCD_init(); // Initialize the LCD and remove the cursor
 	LCD_WriteCommand(0x0C);
-	highscore100 = eeprom_read_byte((uint8_t*)3);
+	
+	highscore100 = eeprom_read_byte((uint8_t*)3); // Read high score from memory
 	highscore10 = eeprom_read_byte((uint8_t*)4);
 	highscore1 = eeprom_read_byte((uint8_t*)5);
 	highscore = eeprom_read_byte((uint8_t*)6);
-	if(highscore100 > 9)
+	if(highscore100 > 9) // Reset high score to 0 if memory has been corrupted
 	{ 
-		eeprom_write_byte((uint8_t*)3, 0);
-		eeprom_write_byte((uint8_t*)4, 0);
-		eeprom_write_byte((uint8_t*)5, 0);
-		eeprom_write_byte((uint8_t*)6, 0);
 		highscore100 = 0;
 		highscore10 = 0;
 		highscore1 = 0;
 		highscore = 0;
 	}
-	LCDBuildChar(1, customChar1); 
-	LCDBuildChar(2, customChar2);
-	LCD_Cursor(1); LCD_WriteData(1);
+	LCDBuildChar(1, customChar1); // Create inverted custom character
+	LCDBuildChar(2, customChar2); // Create non-inverted custom character
+	LCD_Cursor(1); LCD_WriteData(1); // Output high score and custom characters
 	LCD_Cursor(2); LCD_WriteData(highscore100 + '0');
 	LCD_Cursor(3); LCD_WriteData(highscore10 + '0');
 	LCD_Cursor(4); LCD_WriteData(highscore1 + '0');
 	LCD_Cursor(5); LCD_WriteData(2);
-	TimerSet(tasksPeriod);
+	
+	TimerSet(tasksPeriod); // Set the period for the entire system
 	TimerOn();
-
+	
+	// Initialize schedule tasks 
 	unsigned char i = 0;
 	tasks[i].state = -1;
 	tasks[i].period = 50;
@@ -253,6 +274,6 @@ int main(void)
 	tasks[i].period = 1000;
 	tasks[i].elapsedTime = 0;
 	tasks[i].TickFct = &Score_Task;
-    while(1){}
+    while(1){} //Scheduler occurs in TimerISR() function which is called automatically within while loop
 }
 
